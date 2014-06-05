12da74220a249964487bfd91b61bceb882c412b6
Clayton <clayton@xdevel.com.br>
2014-06-05 19:54:53 -0300

    Testado os ambientes de producao e Teste

--- test/app/SpecSettings.scala
+++ test/app/SpecSettings.scala
@@ -5,6 +5,24 @@ package app
  */
 trait SpecSettings {
   val withoutPlugins = Seq(
+//    "com.typesafe.plugin.CommonsMailerPlugin",
+//    "securesocial.core.DefaultAuthenticatorStore",
+//    "securesocial.core.DefaultIdGenerator",
+//    "securesocial.core.providers.utils.DefaultPasswordValidator",
+//    "securesocial.controllers.DefaultTemplatesPlugin",
+//    "services.AuthenticationService",
+//    "securesocial.core.providers.utils.BCryptPasswordHasher",
+//    "securesocial.core.providers.TwitterProvider",
+//    "securesocial.core.providers.FacebookProvider",
+//    "securesocial.core.providers.GoogleProvider",
+//    "securesocial.core.providers.LinkedInProvider",
+//    "securesocial.core.providers.UsernamePasswordProvider",
+//    "securesocial.core.providers.GitHubProvider",
+//    "services.AuthenticationListener",
+//    "securesocial.core.providers.FoursquareProvider",
+//    "securesocial.core.providers.XingProvider",
+//    "securesocial.core.providers.VkProvider",
+//    "securesocial.core.providers.InstagramProvider"
     "com.typesafe.plugin.CommonsMailerPlugin",
     "securesocial.core.DefaultAuthenticatorStore",
     "securesocial.core.DefaultIdGenerator",
@@ -12,15 +30,9 @@ trait SpecSettings {
     "securesocial.controllers.DefaultTemplatesPlugin",
     "services.AuthenticationService",
     "securesocial.core.providers.utils.BCryptPasswordHasher",
-    "securesocial.core.providers.TwitterProvider",
     "securesocial.core.providers.FacebookProvider",
     "securesocial.core.providers.GoogleProvider",
-    "securesocial.core.providers.LinkedInProvider",
     "securesocial.core.providers.UsernamePasswordProvider",
-    "securesocial.core.providers.GitHubProvider",
-    "services.AuthenticationListener",
-    "securesocial.core.providers.FoursquareProvider",
-    "securesocial.core.providers.XingProvider",
-    "securesocial.core.providers.VkProvider",
-    "securesocial.core.providers.InstagramProvider")
+    "services.AuthenticationListener"
+  )
 }
