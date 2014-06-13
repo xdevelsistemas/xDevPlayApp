ed65a3e6895ef2026e8d776a249e44d5bef0055c
Clayton <clayton@xdevel.com.br>
2014-06-13 00:44:12 -0300

    Adicionado alterarcodigo e ajustado o cabecalho logado

--- app/controllers/Proconsorcio/Home.java
+++ app/controllers/Proconsorcio/Home.java
@@ -2,15 +2,34 @@ package controllers.Proconsorcio;
 
 import play.mvc.Result;
 import play.mvc.Controller;
+import securesocial.core.java.SecureSocial;
 
 /**
  * Created by claytonsantosdasilva on 11/06/14.
  */
  public class Home extends Controller{
+
+    @SecureSocial.UserAwareAction
     public static Result home()
     {
-        return ok(views.html.Proconsorcio.index.render());
-        
+
+        dao.UserIdentity user = (dao.UserIdentity) ctx().args.get(SecureSocial.USER_KEY);
+
+        if  (user != null)
+        {
+            return ok(views.html.Proconsorcio.logado.render());
+        }else
+        {
+            return ok(views.html.Proconsorcio.index.render());
+        }
+
+    }
+
+
+    @SecureSocial.SecuredAction
+    public static Result alterarcodigo()
+    {
+        return ok (views.html.Proconsorcio.alterarcodigo.render());
     }
 }
 
