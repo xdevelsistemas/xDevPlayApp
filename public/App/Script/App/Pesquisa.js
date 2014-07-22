0a3654bcc98f2a78b91c257ca1910ade018dc87b
nilo <nilomusso@gmail.com>
2014-07-22 01:49:07 -0300

    primeiro teste com angularjs 1.2 (cdn) funcionando

--- public/App/Script/App/Pesquisa.js
+++ public/App/Script/App/Pesquisa.js
@@ -1,25 +1,34 @@
-/**
- * Created by claytonsantosdasilva on 18/06/14.
- */
-require(['Libs/domReady'], function (domReady) {
-    domReady(function () {
-            require(["jquery","knockout", "komapping"], function ($, ko, mapping) {
-                window.ko = ko;
-                window.ko.mapping = mapping;
+var app = angular.module('App', []);
+app.controller('Filtros', function ($scope) {
+    $scope.tituloCaixaFiltros = "Pesquisar Consórcios";
+    $scope.textoBotaoFiltrar = "Filtrar!";
+    $scope.filtros = [];
+    $scope.pay = function pay() {
+        window.alert("Thanks!");
+    };
 
-                require(["App"], function () {
-                    xapp = new App();
-                    xapp.Modelo.Pesquisa.filtros = xapp.aplicarModelo("Filtros", "#caixa-filtros");
-                    xapp.Modelo.Pesquisa.resultados = xapp.aplicarModelo("ResultadosPesquisa", "#resultadosPesquisa", function () {
-                        $('select[name=filter_sort_by]').change(function () {
-                            $('form.form-sort').submit();
-                        });
-                        $('select[name=filter_order]').change(function () {
-                            $('form.form-sort').submit();
-                        });
-                    });
-                });
-
-            });
-        });
 });
+
+
+//require(['Libs/domReady'], function (domReady) {
+//    domReady(function () {
+//            require(["jquery","knockout", "komapping"], function ($, ko, mapping) {
+//                window.ko = ko;
+//                window.ko.mapping = mapping;
+//
+//                require(["App"], function () {
+//                    xapp = new App();
+//                    xapp.Modelo.Pesquisa.filtros = xapp.aplicarModelo("Filtros", "#caixa-filtros");
+//                    xapp.Modelo.Pesquisa.resultados = xapp.aplicarModelo("ResultadosPesquisa", "#resultadosPesquisa", function () {
+//                        $('select[name=filter_sort_by]').change(function () {
+//                            $('form.form-sort').submit();
+//                        });
+//                        $('select[name=filter_order]').change(function () {
+//                            $('form.form-sort').submit();
+//                        });
+//                    });
+//                });
+//
+//            });
+//        });
+//});
