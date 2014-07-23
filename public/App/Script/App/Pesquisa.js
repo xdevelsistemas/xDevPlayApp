c3b27cb51a3071cf245fcda340836f151348298f
nilo <nilomusso@gmail.com>
2014-07-22 23:21:52 -0300

    página de pesquisa somente com angularjs

--- public/App/Script/App/Pesquisa.js
+++ public/App/Script/App/Pesquisa.js
@@ -1,34 +1,17 @@
-var app = angular.module('App', []);
-app.controller('Filtros', function ($scope) {
-    $scope.tituloCaixaFiltros = "Pesquisar Consórcios";
-    $scope.textoBotaoFiltrar = "Filtrar!";
-    $scope.filtros = [];
-    $scope.pay = function pay() {
-        window.alert("Thanks!");
-    };
-
-});
-
-
-//require(['Libs/domReady'], function (domReady) {
-//    domReady(function () {
-//            require(["jquery","knockout", "komapping"], function ($, ko, mapping) {
-//                window.ko = ko;
-//                window.ko.mapping = mapping;
-//
-//                require(["App"], function () {
-//                    xapp = new App();
-//                    xapp.Modelo.Pesquisa.filtros = xapp.aplicarModelo("Filtros", "#caixa-filtros");
-//                    xapp.Modelo.Pesquisa.resultados = xapp.aplicarModelo("ResultadosPesquisa", "#resultadosPesquisa", function () {
-//                        $('select[name=filter_sort_by]').change(function () {
-//                            $('form.form-sort').submit();
-//                        });
-//                        $('select[name=filter_order]').change(function () {
-//                            $('form.form-sort').submit();
-//                        });
-//                    });
-//                });
-//
-//            });
-//        });
-//});
+angular.module('Pesquisa', [])
+    .controller('Filtros', function ($scope, $http) {
+        $http.get('/assets/App/Mockup/Filtros.json').success(function (data) {
+            angular.extend($scope, data);
+        });
+        $scope.filtrar = function () {
+            window.alert("Thanks!");
+        };
+    })
+    .controller('Resultados', function ($scope, $http) {
+        $http.get('/assets/App/Mockup/ResultadosPesquisa.json').success(function (data) {
+            angular.extend($scope, data);
+        });
+        $scope.filtrar = function () {
+            window.alert("Thanks!");
+        };
+    });
