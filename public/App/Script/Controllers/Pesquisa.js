7b66720ccc2950a20fd283f02ef720997daaceac
Nilo Musso <nilomusso@gmail.com>
2014-07-25 00:04:25 -0300

    métodos de mudar as páginas implementado nas views

--- public/App/Script/Controllers/Pesquisa.js
+++ public/App/Script/Controllers/Pesquisa.js
@@ -2,7 +2,6 @@ define(['./__module__', 'jquery'], function (controllers, $) {
     'use strict';
     controllers.controller('Pesquisa', ['$scope', '$http', 'Location', function ($scope, $http, Location) {
         var QUERY_FILTROS = window.location.search;
-        console.log('QUERY_FILTROS', ':', QUERY_FILTROS);
 
         $http.get('/assets/App/Mockup/Filtros.json' + QUERY_FILTROS).success(function (data) {
             angular.extend($scope, data);
@@ -13,44 +12,36 @@ define(['./__module__', 'jquery'], function (controllers, $) {
 
         $scope.buscarResultados = function () {
             var objQuery = Location.toObject(QUERY_FILTROS);
-            console.log('objQuery', objQuery);
-            var query = $.param(angular.extend(objQuery), {
+            var query = $.param(angular.extend(objQuery, {
                 ordenador: $scope.ordenadores.selecionado,
                 ordem: $scope.ordem.selecionada,
                 pagina: $scope.paginas.selecionada
-            });
-            console.log(query);
+            }));
             $http.get('/assets/App/Mockup/ResultadosPesquisa.json' + '?' + query).success(function (data) {
                 angular.extend($scope, data);
             });
         };
 
         $scope.ordenarResultado = function () {
-            $scope.paginas.selecionada = '1';
+            $scope.paginas.selecionada = 1;
             $scope.buscarResultados();
         }
 
         $scope.passarPagina = function () {
             var p = parseInt($scope.paginas.selecionada);
             var t = parseInt($scope.paginas.total);
-            console.log('p', p);
             if (p < t) {
-                console.log('menor que o total(' + t + ')');
                 $scope.paginas.selecionada = p + 1;
                 $scope.buscarResultados();
             }
-            console.log('selecionada', $scope.paginas.selecionada);
         }
 
         $scope.voltarPagina = function () {
             var p = parseInt($scope.paginas.selecionada);
-            console.log('p', p);
             if (p > 1) {
-                console.log('maior que 1');
                 $scope.paginas.selecionada = p - 1;
                 $scope.buscarResultados();
             }
-            console.log('selecionada', $scope.paginas.selecionada);
         }
 
         $scope.ultimaPagina = function () {
@@ -64,20 +55,3 @@ define(['./__module__', 'jquery'], function (controllers, $) {
 
     }]);
 });
-
-
-/*
-
- {
- "codigo": ,
- "descricao": ,
- }
-
- {
- "ordenador": "preco",
- "ordem": "asc",
- "pagina": "1"
- }
-
-
- * */
