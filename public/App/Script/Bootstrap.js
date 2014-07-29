706863822735646aefd1713b7f0ed905e584ea04
Nilo Musso <nilomusso@gmail.com>
2014-07-29 18:06:41 -0300

    escritório online: painéis e datatables

--- public/App/Script/Bootstrap.js
+++ public/App/Script/Bootstrap.js
@@ -6,6 +6,7 @@ define([
     'require',
     'angular',
     'jquery',
+    'bootstrap',
     'datatables',
     'jqueryPlugins',
     'App',
--- public/App/Script/Directives/xdDataTable.js
+++ public/App/Script/Directives/xdDataTable.js
@@ -1,11 +1,35 @@
-define(['./__module__', 'jquery'], function (directives) {
+define(['./__module__', 'jquery', 'datatables'], function (directives, $) {
     'use strict';
     directives.directive('xdDataTable', [function () {
         return {
             restrict: 'A',
             link: function (scope, elm, attr) {
                 window.setTimeout(function () {
-                    //elm.dataTable();
+                    $(elm[0]).dataTable({
+                        language: {
+                            "sEmptyTable": "Nenhum registro encontrado",
+                            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                            "sInfoPostFix": "",
+                            "sInfoThousands": ".",
+                            "sLengthMenu": "_MENU_ resultados por página",
+                            "sLoadingRecords": "Carregando...",
+                            "sProcessing": "Processando...",
+                            "sZeroRecords": "Nenhum registro encontrado",
+                            "sSearch": "Pesquisar",
+                            "oPaginate": {
+                                "sNext": "Próximo",
+                                "sPrevious": "Anterior",
+                                "sFirst": "Primeiro",
+                                "sLast": "Último"
+                            },
+                            "oAria": {
+                                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                                "sSortDescending": ": Ordenar colunas de forma descendente"
+                            }
+                        }
+                    });
                 }, 20);
             }
         };
