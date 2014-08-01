95c4e6964375b8bd47a54c05bf6d028224f2417e
Clayton <clayton@xdevel.com.br>
2014-08-01 12:54:37 -0300

    encapsulado melhor as transações da sessão no banco

--- app/controllers/Proconsorcio/Application.scala
+++ app/controllers/Proconsorcio/Application.scala
@@ -7,15 +7,14 @@ import com.typesafe.plugin._
 import play.api.data.Forms._
 import play.api.i18n.Messages
 import play.api.libs.json._
-import java.text.{SimpleDateFormat}
+import java.text.SimpleDateFormat
 import br.com.republicavirtual.CepService
 import controllers.xDevController
-import dao.{IdentityDAO}
-import models.Cadastro.{ RegistrationObjects, AlterarDadosInfo}
+import dao.IdentityDAO
+import models.Cadastro.{RegistrationObjects, AlterarDadosInfo}
 import models.User
 import play.api.data._
-import play.api.mvc.{Action}
-import play.db.jpa.Transactional
+import play.api.mvc.Action
 import play.libs.F
 import securesocial.controllers.Registration
 import securesocial.core.providers.utils.PasswordValidator
@@ -25,6 +24,10 @@ import play.db.jpa.JPA
 
 /**
  * Created by claytonsantosdasilva on 10/07/14.
+ * Controller responsável pelo processamento das páginas dentro da aplicação proconsórcio
+ *
+ * Desenvolvido pela xDevel Sistemas
+ *
  */
 object Application extends xDevController{
   val NumCodigo = "numCodigo"
@@ -77,15 +80,15 @@ object Application extends xDevController{
     val usuario: User = (new IdentityDAO).findOneByEmailAndProvider( _user.get.email.get,_user.get.identityId.providerId).user()
     val regdata: AlterarDadosInfo = new AlterarDadosInfo(
       usuario.realName,
-      if (usuario.birthDate == null) (new String("")) else (new SimpleDateFormat("dd/MM/yyyy")).format(usuario.birthDate),
-      (new Some[String](if (usuario.numCep == null || usuario.numCep.equals("") ) (new String("")) else usuario.numCep)),
-      if (usuario.siglaUf == null || usuario.siglaUf.equals("") )  (new String("")) else usuario.siglaUf,
-      if (usuario.nomeCidade == null || usuario.nomeCidade.equals("") )  (new String("")) else usuario.nomeCidade,
-      if (usuario.nomeBairro == null || usuario.nomeBairro.equals("") )  (new String("")) else usuario.nomeBairro,
-      if (usuario.nomeLogradouro == null || usuario.nomeLogradouro.equals("") )  (new String("")) else usuario.nomeLogradouro,
-      if (usuario.numLogradouro == null || usuario.numLogradouro.equals("") )  (new String("")) else usuario.numLogradouro,
-      (new Some[String](if (usuario.numRg == null || usuario.numRg.equals("") )  (new String("")) else usuario.numRg)),
-      if (usuario.numDocFederal == null || usuario.numDocFederal.equals("") )  (new String("")) else usuario.numDocFederal)
+      if (usuario.birthDate == null) new String("") else new SimpleDateFormat("dd/MM/yyyy").format(usuario.birthDate),
+      new Some[String](if (usuario.numCep == null || usuario.numCep.equals("") ) new String("") else usuario.numCep),
+      if (usuario.siglaUf == null || usuario.siglaUf.equals("") )  new String("") else usuario.siglaUf,
+      if (usuario.nomeCidade == null || usuario.nomeCidade.equals("") )  new String("") else usuario.nomeCidade,
+      if (usuario.nomeBairro == null || usuario.nomeBairro.equals("") )  new String("") else usuario.nomeBairro,
+      if (usuario.nomeLogradouro == null || usuario.nomeLogradouro.equals("") )  new String("") else usuario.nomeLogradouro,
+      if (usuario.numLogradouro == null || usuario.numLogradouro.equals("") )  new String("") else usuario.numLogradouro,
+      new Some[String](if (usuario.numRg == null || usuario.numRg.equals("") )  new String("") else usuario.numRg),
+      if (usuario.numDocFederal == null || usuario.numDocFederal.equals("") )  new String("") else usuario.numDocFederal)
     val userForm: Form[AlterarDadosInfo] = models.Cadastro.RegistrationObjects.formAlterarDados.fill(regdata)
 
 
@@ -143,14 +146,13 @@ object Application extends xDevController{
       },
 
       success => {
-        JPA.withTransaction("default", false, new F.Function0[Void] {
-          def apply: Void = {
-            _userdao.alterarCadastro(((RegistrationObjects.formAlterarDados)bindFromRequest()).value,_user.get.email.get, _user.get.identityId.providerId)
-            return null
+        JPA.withTransaction(new F.Function0[Void] {
+          def apply: Unit = {
+             _userdao.alterarCadastro(RegistrationObjects.formAlterarDados.bindFromRequest().value,_user.get.email.get, _user.get.identityId.providerId)
           }
         })
 
-        play.api.mvc.Results.Ok(views.html.App.main.render(views.html.Proconsorcio.dadoscadastrais((RegistrationObjects.formAlterarDados)bindFromRequest(),"Dados Alterados com Sucesso!"), "Dados Cadastrais",_user,request))
+        play.api.mvc.Results.Ok(views.html.App.main.render(views.html.Proconsorcio.dadoscadastrais(RegistrationObjects.formAlterarDados.bindFromRequest(),"Dados Alterados com Sucesso!"), "Dados Cadastrais",_user,request))
 
 
 
@@ -176,13 +178,13 @@ object Application extends xDevController{
         mapping(
           CodigoAtual -> text.verifying(
             Messages(InvalidPasswordMessage), checkCurrentPassword(_user.get.email.get,_user.get.identityId.providerId ,_)),
-          (NumCodigo ->
+          NumCodigo ->
             tuple(
               Password1 -> nonEmptyText.verifying(use[PasswordValidator].errorMessage,
                 p => use[PasswordValidator].isValid(p)),
               Password2 -> nonEmptyText
             ).verifying(Messages(Registration.PasswordsDoNotMatch), passwords => passwords._1 == passwords._2)
-            )
+
         )
         ((codigoAtual, numCodigo) => models.Cadastro.AlteraCodigoInfo(Some(codigoAtual), numCodigo._1)
           )
@@ -197,11 +199,11 @@ object Application extends xDevController{
       },
 
       success => {
-        //TODO encapsular persistencias
-        JPA.withTransaction("default", false, new F.Function0[Void] {
-          def apply: Void = {
+
+        JPA.withTransaction(new F.Function0[Void] {
+          def apply: Unit = {
             _userdao.AlteranumCodigo(_user.get.email.get,_user.get.identityId.providerId, formAlterarCodigo.bindFromRequest().value.get.numCodigo)
-            return null
+
           }
         })
         Ok(views.html.App.main.render(views.html.Proconsorcio.alterarcodigo(RegistrationObjects.formAlterarCodigo,"Código Alterado com Sucesso!"), "Alterar Código de Compra/Venda", _user,request))
