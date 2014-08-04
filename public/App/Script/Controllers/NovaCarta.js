d64eb564ee034181f116f6da702556d8589347d5
Nilo Musso <nilomusso@gmail.com>
2014-08-04 19:34:11 -0300

    nova carta: preparado pra requisitar para o rest

--- public/App/Script/Controllers/NovaCarta.js
+++ public/App/Script/Controllers/NovaCarta.js
@@ -6,63 +6,73 @@ define(['./__module__', 'jquery'], function (controllers, $) {
         $scope.administradora = {};
         $scope.contemplacao = {};
         $scope.contas = {};
-
+        $scope.formData = {
+            "resp": {
+                "result": "",
+                "message": ""
+            }
+        };
+        //Funções
         function inicializarModeloForm() {
-            $scope.formData = {
-                "resp": {
-                    "result": "",
-                    "message": ""
+            $scope.formData.fields = {
+                "tipo": {
+                    "value": "",
+                    "error": ""
                 },
-                "fields": {
-                    "tipo": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "administradora": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "contemplacao": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "prazoRestante": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "valorCredito": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "valorEntrada": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "valorPrestacoes": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "cota": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "conta": {
-                        "value": "",
-                        "error": ""
-                    },
-                    "numCodigo": {
-                        "value": "",
-                        "error": ""
-                    }
+                "administradora": {
+                    "value": "",
+                    "error": ""
+                },
+                "contemplacao": {
+                    "value": "",
+                    "error": ""
+                },
+                "prazoRestante": {
+                    "value": "",
+                    "error": ""
+                },
+                "valorCredito": {
+                    "value": "",
+                    "error": ""
+                },
+                "valorEntrada": {
+                    "value": "",
+                    "error": ""
+                },
+                "valorPrestacoes": {
+                    "value": "",
+                    "error": ""
+                },
+                "cota": {
+                    "value": "",
+                    "error": ""
+                },
+                "conta": {
+                    "value": "",
+                    "error": ""
+                },
+                "numCodigo": {
+                    "value": "",
+                    "error": ""
                 }
             };
         };
-        inicializarModeloForm();
-
         function naoEeNum(s) {
             return s === undefined || s === null || isNaN(parseFloat(s.replace('R$ ', '').split('.').join('').replace(',', '.')));
         };
+        function formatResult(item) {
+            return item.text.replace('__ABRE__', '<span class="select-conta-padrao"><i class="icon-white icon-star"></i> (Padrão) ')
+                .replace('__FECHA__', '</span>');
+        };
+        function formatSelection(item) {
+            return item.text.replace('__ABRE__', '<span class="select-conta-padrao"><i class="icon icon-star"></i> (Padrão) ')
+                .replace('__FECHA__', '</span>');
 
+        };
+        function aplicarSelect2Contas(elm_name, again) {
+            if (again) $(elm_name).select2("destroy")
+            $(elm_name).select2({'width': '100%', formatResult: formatResult, formatSelection: formatSelection});
+        };
         $scope.formatConta = function (conta) {
             var abre = '';
             var fecha = '';
@@ -72,25 +82,6 @@ define(['./__module__', 'jquery'], function (controllers, $) {
             }
             return abre + 'AG: ' + conta.agencia + ' / ' + 'CC: ' + conta.conta + fecha;
         };
-
-        function formatResult(item) {
-            return item.text.replace('__ABRE__', '<span class="select-conta-padrao"><i class="icon-white icon-star"></i> (Padrão) ')
-                .replace('__FECHA__', '</span>');
-
-        }
-
-        function formatSelection(item) {
-            return item.text.replace('__ABRE__', '<span class="select-conta-padrao"><i class="icon icon-star"></i> (Padrão) ')
-                .replace('__FECHA__', '</span>');
-
-        }
-
-        function aplicarSelect2Contas(elm_name, again) {
-            if (again) $(elm_name).select2("destroy")
-            $(elm_name).select2({'width': '100%', formatResult: formatResult, formatSelection: formatSelection});
-        };
-
-
         $scope.enviarFormNovaCarta = function (form, isInvalid) {
             console.log('>>>', $scope.formData);
             $scope.formData.fields.tipo.error = form.tipo.$invalid ? $scope.strings.campoObrigatorio : null;
@@ -105,7 +96,6 @@ define(['./__module__', 'jquery'], function (controllers, $) {
             console.log("formulário válido!");
             $("#modal-selecionarConta").modal('show');
         };
-
         $scope.enviarFormConta = function (form, isInvalid) {
             $scope.formData.fields.conta.error = form.conta.$invalid ? $scope.strings.campoObrigatorio : null;
             $scope.formData.fields.numCodigo.error = form.numCodigo.$invalid ? $scope.strings.campoObrigatorio : null;
@@ -115,18 +105,17 @@ define(['./__module__', 'jquery'], function (controllers, $) {
                 return;
             }
             console.log("formulário válido!");
-            inicializarModeloForm();
-            $("#modal-selecionarConta").modal('hide');
-            //TODO  retirar este mockup
-            $scope.formData = {
-                "resp": {
-                    "result": "1",
-                    "message": $sce.trustAsHtml('Carta adicionada com sucesso! Acesse o <a href="/escritorio"><strong>Escritório Online</strong></a> para maiores detalhes.')
+            $http.post("url").success(function (data) {
+                angular.extend($scope.contemplacao, data);
+                if ($scope.resp.result == '1') {
+                    inicializarModeloForm();
+                    $("#modal-selecionarConta").modal('hide');
+                    $scope.formData.resp.message = $sce.trustAsHtml('Carta adicionada com sucesso! Acesse o <a href="/escritorio"><strong>Escritório Online</strong></a> para maiores detalhes.');
                 }
-            };
+            });
         };
-
         //Inicializador
+        inicializarModeloForm();
         $http.get("/assets/App/Mockup/NovaCarta/strings.json").success(function (data) {
             angular.extend($scope.strings, data);
         });
@@ -143,8 +132,5 @@ define(['./__module__', 'jquery'], function (controllers, $) {
             angular.extend($scope.contas, data);
             aplicarSelect2Contas("select[name='conta']");
         });
-
-    }
-    ])
-    ;
+    }]);
 });
